10e23ce6fef7b359b30f3bec9746f58a0e730b90
dsoegaard <dsoegaard@4e58de57-8926-0410-947e-8945c843cdd7>
2008-11-13 12:32:36 +0000

    Integration test cases for lookup with processes finished and working.
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@16095 4e58de57-8926-0410-947e-8945c843cdd7

--- test/dk.gov.oiosi.integration/uddi/UddiLookupClientTest.cs
+++ test/dk.gov.oiosi.integration/uddi/UddiLookupClientTest.cs
@@ -13,7 +13,7 @@ namespace dk.gov.oiosi.integration.uddi {
             public const string SERVICEIDENTIFIER = "5798009811578";
             public const string ORDERSERVICETYPEID = "uddi:b138dc71-d301-42d1-8c2e-2c3a26faf56a";
             public const string SIMPLEBILLINGPROCESSID = "uddi:98070e14-ee30-4b10-84ef-986cde3b8116";
-            public const string SIMPLEORDERPROCESSID = "uddi:1e1b209e-7b8d-4f1e-8f2a-f3d9a94d1086";
+            public const string SIMPLEORDERPROCESSID = "uddi:142c4188-3d53-440d-a64f-68d7c3b9a59b";
             public const string NESPROFILE5PROCESSID = "uddi:aee8b6de-298f-4cbc-a96d-9ae8aed0ac31";
             
             [Test]
@@ -34,6 +34,9 @@ namespace dk.gov.oiosi.integration.uddi {
                 processDefinitionIds.Add(new UddiGuidId(SIMPLEORDERPROCESSID, true));
                 List<UddiLookupResponse> responses = Lookup(identifier, serviceDefinitionId, processDefinitionIds);
                 Assert.AreEqual(1, responses.Count);
+                foreach (UddiProcessInformation process in responses[0].Processes) {
+                    Assert.AreEqual(SIMPLEORDERPROCESSID, process.ProcessDefinitionId);
+                }
             }
 
             [Test]
@@ -56,6 +59,9 @@ namespace dk.gov.oiosi.integration.uddi {
                 processDefinitionIds.Add(new UddiGuidId(SIMPLEORDERPROCESSID, true));
                 List<UddiLookupResponse> responses = Lookup(identifier, serviceDefinitionId, processDefinitionIds);
                 Assert.AreEqual(1, responses.Count);
+                foreach (UddiProcessInformation process in responses[0].Processes) {
+                    Assert.AreEqual(SIMPLEORDERPROCESSID, process.ProcessDefinitionId);
+                }
             }
 
 
