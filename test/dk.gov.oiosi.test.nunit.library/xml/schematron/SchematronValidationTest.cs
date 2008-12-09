7c2cd4aa7271d9776a6f6b06e2c7d451ff1be9ef
jjensen <jjensen@4e58de57-8926-0410-947e-8945c843cdd7>
2008-12-09 13:58:47 +0000

    Schematron validation added for the remaining oioubl documents
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@16240 4e58de57-8926-0410-947e-8945c843cdd7

--- test/dk.gov.oiosi.test.nunit.library/xml/schematron/SchematronValidationTest.cs
+++ test/dk.gov.oiosi.test.nunit.library/xml/schematron/SchematronValidationTest.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Xml;
 
 using NUnit.Framework;
@@ -21,17 +19,41 @@ namespace dk.gov.oiosi.test.nunit.library.xml.schematron {
         }
 
         [Test]
-        public void OioublInvoiceValidation() {
-            DocumentTypeConfig documentType = _defaultDocumentTypes.Invoice();
-            Validate(TestConstants.PATH_INVOICE_XML, documentType);
-        }
-
-        [Test]
         public void OioublApplicationResponseValidation() {
             DocumentTypeConfig documentType = _defaultDocumentTypes.ApplicationResponse();
             Validate(TestConstants.PATH_APPLICATIONRESPONSE_XML, documentType);
         }
 
+        [Test]
+        public void OioublCatalogueValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.Catalogue();
+            Validate(TestConstants.PATH_CATALOGUE_XML, documentType);
+        }
+
+        [Test]
+        public void OioublCatalogueDeletionValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.CatalogueDeletion();
+            Validate(TestConstants.PATH_CATALOGUEDELETION_XML, documentType);
+        }
+        
+        [Test]
+        public void OioublCatalogueItemSpecificationUpdateValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.CatalogueItemSpecificationUpdate();
+            Validate(TestConstants.PATH_CATALOGUEITEMSPECIFICATIONUPDATE_XML, documentType);
+        }
+        
+        [Test]
+        public void OioublCataloguePricingUpdateValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.CataloguePricingUpdate();
+            Validate(TestConstants.PATH_CATALOGUEPRICINGUPDATE_XML, documentType);
+        }
+        
+        [Test]
+        public void OioublCatalogueRequestValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.CatalogueRequest();
+            Validate(TestConstants.PATH_CATALOGUEREQUEST_XML, documentType);
+        }
+        
         [Test]
         public void OioublCreditNoteValidation() {
             DocumentTypeConfig documentType = _defaultDocumentTypes.CreditNote();
@@ -39,9 +61,33 @@ namespace dk.gov.oiosi.test.nunit.library.xml.schematron {
         }
 
         [Test]
+        public void OioublInvoiceValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.Invoice();
+            Validate(TestConstants.PATH_INVOICE_XML, documentType);
+        }
+
+        [Test]
         public void OioublOrderValidation() {
             DocumentTypeConfig documentType = _defaultDocumentTypes.Order();
             Validate(TestConstants.PATH_ORDER_XML, documentType);
+        }
+
+        [Test]
+        public void OioublOrderCancellationValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.OrderCancellation();
+            Validate(TestConstants.PATH_ORDERCANCELLATION_XML, documentType);
+        }
+
+        [Test]
+        public void OioublOrderChangeValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.OrderChange();
+            Validate(TestConstants.PATH_ORDERCHANGE_XML, documentType);
+        }
+
+        [Test]
+        public void OioublOrderResponseValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.OrderResponse();
+            Validate(TestConstants.PATH_ORDERRESPONSE_XML, documentType);
         }
 
         [Test]
@@ -56,7 +102,13 @@ namespace dk.gov.oiosi.test.nunit.library.xml.schematron {
             Validate(TestConstants.PATH_REMINDER_XML, documentType);
         }
 
-        [Test, ExpectedException(typeof(dk.gov.oiosi.xml.schematron.SchematronErrorException))]
+        [Test]
+        public void OioublStatementValidation() {
+            DocumentTypeConfig documentType = _defaultDocumentTypes.Statement();
+            Validate(TestConstants.PATH_STATEMENT_XML, documentType);
+        }
+
+        [Test, ExpectedException(typeof(SchematronErrorException))]
         public void OioublNoIdInvoiceValidation() {
             DocumentTypeConfig documentType = _defaultDocumentTypes.Invoice();
             Validate(TestConstants.PATH_INVOICENOID_XML, documentType);
@@ -68,7 +120,7 @@ namespace dk.gov.oiosi.test.nunit.library.xml.schematron {
             Validate(TestConstants.PATH_INVOICE07_XML, documentType);
         }
 
-        [Test, ExpectedException(typeof(dk.gov.oiosi.xml.schematron.SchematronErrorException))]
+        [Test, ExpectedException(typeof(SchematronErrorException))]
         public void OioxmlInvalidEanNumberInvoiceValidation() {
             DocumentTypeConfig documentType = _defaultDocumentTypes.InvoiceV07();
             Validate(TestConstants.PATH_INVOICEN7INVALIDEANNUMBER_XML, documentType);
