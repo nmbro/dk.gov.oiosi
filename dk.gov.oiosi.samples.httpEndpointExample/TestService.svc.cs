4cb4fea3dc999123f86bea1243f6b00dbe95ed48
pjohansson <pjohansson@4e58de57-8926-0410-947e-8945c843cdd7>
2009-01-23 09:40:06 +0000

    Added new HTTP endpoint sample, that is set up as a web service project
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@16450 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ dk.gov.oiosi.samples.httpEndpointExample/TestService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Channels;
+using dk.gov.oiosi.communication.service;
+
+namespace dk.gov.oiosi.samples.httpEndpointExample
+{
+
+    /// <summary>
+    /// The service implementation
+    /// Implements the general RASP contract and takes any form of SOAP (hence the Message object as a parameter)
+    /// </summary>
+    public class TestService : IServiceContract
+    {
+        public Message RequestRespond(Message request)
+        {
+            // Return the same message you got
+            return request;
+        }
+    }
+}
