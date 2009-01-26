a57695ed00f0918af3a9dda25fbed766b70cfce6
pjohansson <pjohansson@4e58de57-8926-0410-947e-8945c843cdd7>
2009-01-26 10:06:10 +0000

    Integration
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@16462 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ test/dk.gov.oiosi.integration/communication/RequestTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using dk.gov.oiosi.communication;
+using System.Xml;
+using System.ServiceModel.Channels;
+
+namespace dk.gov.oiosi.integration.communication
+{
+    [TestFixture]
+    public class RequestTest
+    {
+        [Test]
+        public void TestHttpRequest()
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load("../../resources/Invoice.2.01.xml");
+            
+            OiosiMessage message = new OiosiMessage(xdoc);
+            AddMandatoryRaspHeaders(message);
+            
+            Response response;
+            Request request = new Request("OiosiHttpEndpoint");
+            request.GetResponse(message, out response);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.ResponseMessage);
+            Assert.IsNotNull(response.ResponseMessage.MessageId);
+        }
+       
+
+        /// <summary>
+        /// Adds the MessageIdentifier
+        /// </summary>
+        public static void AddMandatoryRaspHeaders(OiosiMessage msg)
+        {
+            // Set the name of the header
+            XmlQualifiedName headerName = new XmlQualifiedName("MessageIdentifier", common.Definitions.DefaultOiosiNamespace2007);
+
+            // Create a WCF header
+            MessageHeader header = MessageHeader.CreateHeader(headerName.Name, headerName.Namespace, "1234567890");
+
+            // Add it to our OIOSI Message
+            msg.MessageHeaders.Add(headerName, header);
+        }
+    }
+}
