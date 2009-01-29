25a954102855c153e212b599ffbaac003197747f
cpedersen <cpedersen@4e58de57-8926-0410-947e-8945c843cdd7>
2009-01-29 18:10:00 +0000

    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@16547 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ test/dk.gov.oiosi.integration/security/revocation/CrlLookupTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using dk.gov.oiosi.security.revocation;
+using dk.gov.oiosi.security.revocation.crl;
+using NUnit.Framework;
+
+namespace dk.gov.oiosi.test.integration.security.revocation
+{
+    [TestFixture]
+    public class CrlLookupTest
+    {
+
+        [Test]
+        public void LookupTestValidCertificate()
+        {
+            CrlLookup ocspLookup = new CrlLookup();
+            X509Certificate2 certificate = new X509Certificate2("resources/NemHandel test service.cer");
+            RevocationResponse response = ocspLookup.CheckCertificate(certificate);
+            Assert.IsTrue(response.IsValid);
+        }
+
+        [Test]
+        public void LookupTestRevokedCertificate()
+        {
+            CrlLookup ocspLookup = new CrlLookup();
+            X509Certificate2 certificate = new X509Certificate2("resources/Revoked.cer");
+            try {
+                RevocationResponse response = ocspLookup.CheckCertificate(certificate);
+                Assert.IsTrue(!response.IsValid);
+            }
+            catch (Exception e)
+            {
+                // Certificate is revoked. How do we catch the CertificateRevokedException here?
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ test/dk.gov.oiosi.integration/security/revocation/OcspLookupTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using dk.gov.oiosi.security.revocation;
+using dk.gov.oiosi.security.revocation.ocsp;
+using NUnit.Framework;
+using System.Security.Cryptography.X509Certificates;
+
+namespace dk.gov.oiosi.test.integration.security.revocation {
+
+    [TestFixture]
+    public class OcspLookupTest {
+
+        [Test]
+        public void LookupTestWithoutOcspServerFromCertificate(){
+            OcspConfig ocspConfig = new OcspConfig();
+            ocspConfig.DefaultTimeoutMsec = 10000;
+            X509Certificate2 rootcert = new X509Certificate2("resources/ocesca.cer");
+            OcspLookup ocspLookup = new OcspLookup(ocspConfig, rootcert);
+            X509Certificate2 certificate = new X509Certificate2("resources/NemHandel test service.cer");
+            RevocationResponse response = ocspLookup.CheckCertificate(certificate);
+            Assert.IsTrue(response.IsValid);
+        }
+
+        [Test]
+        public void LookupTestWithoutOcspServerFromConfig() {
+            OcspConfig ocspConfig = new OcspConfig();
+            ocspConfig.DefaultTimeoutMsec = 10000;
+            ocspConfig.ServerUrl = "http://ocsp.certifikat.dk/ocsp/status";
+            X509Certificate2 rootcert = new X509Certificate2("resources/ocesca.cer");
+            OcspLookup ocspLookup = new OcspLookup(ocspConfig, rootcert);
+            X509Certificate2 certificate = new X509Certificate2("resources/NemHandel test service.cer");
+            RevocationResponse response = ocspLookup.CheckCertificate(certificate);
+            Assert.IsTrue(response.IsValid);
+        }
+    }
+}
