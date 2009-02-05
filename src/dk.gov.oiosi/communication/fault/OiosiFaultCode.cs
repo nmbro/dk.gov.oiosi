88da605d0fa19798a2b0ee514040040c9cc28263
pjohansson <pjohansson@4e58de57-8926-0410-947e-8945c843cdd7>
2009-02-05 11:49:58 +0000

    Fault code enumerations separated
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@16601 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ src/dk.gov.oiosi/communication/fault/OiosiFaultCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dk.gov.oiosi.communication.fault
+{
+    /// <summary>
+    /// enum representing faultcode
+    /// </summary>
+    public enum OiosiFaultCode
+    {
+        /// <summary>
+        /// Sender fault source
+        /// </summary>
+        Sender,
+        /// <summary>
+        /// Receiver fault source
+        /// </summary>
+        Receiver
+    };
+}
--- src/dk.gov.oiosi/communication/fault/OiosiMessageFault.cs
+++ src/dk.gov.oiosi/communication/fault/OiosiMessageFault.cs
@@ -46,62 +46,6 @@ namespace dk.gov.oiosi.communication.fault {
         private FaultReason _reason;
 
         /// <summary>
-        /// enum representing faultcode
-        /// </summary>
-        public enum OiosiFaultCode { 
-            /// <summary>
-            /// Sender fault source
-            /// </summary>
-            Sender, 
-            /// <summary>
-            /// Receiver fault source
-            /// </summary>
-            Receiver 
-        };
-        
-        /// <summary>
-        /// enum representing inner faultcode
-        /// </summary>
-        public enum OiosiInnerFaultCode {
-            /// <summary>
-            /// Fault returned when schema validation fails
-            /// </summary>
-            SchemaValidationFault, 
-            /// <summary>
-            /// Fault returned when schematron validation fails
-            /// </summary>
-            SchematronValidationFault, 
-            /// <summary>
-            /// Fault returned when the SOAP message signature isn't valid
-            /// </summary>
-            SignatureNotValidFault, 
-            /// <summary>
-            /// Fault returned when the document isn't of a known UBL type
-            /// </summary>
-            UnknownDocumentTypeFault, 
-            /// <summary>
-            /// Fault returned when message couldn't be persisted
-            /// </summary>
-            MessagePersistencyFault, 
-            /// <summary>
-            /// Fault returned when xml transformation fails
-            /// </summary>
-            XsltTransformationFault, 
-            /// <summary>
-            /// Fault returned when an unknown internal failure occurs
-            /// </summary>
-            InternalSystemFailureFault,
-            /// <summary>
-            /// Fault returned when an obligatory header is missing
-            /// </summary>
-            MissingHeaderFault,
-            /// <summary>
-            /// Fault returned when the sender is not authorized in the current context
-            /// </summary>
-            NotAuthorizedFault
-        };
-
-        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="e">exception</param>
@@ -165,7 +109,7 @@ namespace dk.gov.oiosi.communication.fault {
         }
 
         private FaultCode CreateSenderFaultCode(OiosiInnerFaultCode innerFaultCode) {
-            return new FaultCode(innerFaultCode.ToString());
+            return new FaultCode( (innerFaultCode.ToString());
         }
 
         private FaultCode CreateRecieverFaultCode(OiosiInnerFaultCode innerFaultCode) {
