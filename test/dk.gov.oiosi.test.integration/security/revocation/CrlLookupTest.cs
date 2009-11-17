ed9a0b380e2aae44ce6dd1d5e9bdaa10091b999a
dsoegaard <dsoegaard@4e58de57-8926-0410-947e-8945c843cdd7>
2009-11-17 13:40:31 +0000

    Added multi threaded to of CRL checks.
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@17985 4e58de57-8926-0410-947e-8945c843cdd7

--- test/dk.gov.oiosi.test.integration/security/revocation/CrlLookupTest.cs
+++ test/dk.gov.oiosi.test.integration/security/revocation/CrlLookupTest.cs
@@ -3,6 +3,8 @@ using System.Security.Cryptography.X509Certificates;
 using dk.gov.oiosi.security.revocation;
 using dk.gov.oiosi.security.revocation.crl;
 using NUnit.Framework;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace dk.gov.oiosi.test.integration.security.revocation
 {
@@ -26,6 +28,44 @@ namespace dk.gov.oiosi.test.integration.security.revocation
             X509Certificate2 certificate = new X509Certificate2("Resources/Certificates/Revoked.cer");
             RevocationResponse response = crlLookup.CheckCertificate(certificate);
             Assert.IsFalse(response.IsValid);
+        }
+
+        [Test]
+        public void LookTestMultiThread() {
+            List<Thread> threads = new List<Thread>();
+            CrlLookup crlLookup = new CrlLookup();
+            for (int i = 0; i < 32; i++) {
+                Thread thread = new Thread(ThreadCertificateCheck);
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            Predicate<Thread> isDead = delegate(Thread t) { return !t.IsAlive; };
+
+            while (!threads.TrueForAll(isDead)) {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private void ThreadCertificateCheck() {
+            CrlLookup crlLookup = new CrlLookup();
+            for (int i = 0; i < 16; i++) {
+                Random random = new Random();
+                int select = random.Next(2);
+                if (select < 1) {
+                    Console.WriteLine("{0} ThreadCertificateCheck number:{1} certificate:0", DateTime.Now, i);
+                    X509Certificate2 certificate = new X509Certificate2("Resources/Certificates/FOCES1.pkcs12", "Test1234");
+                    RevocationResponse response = crlLookup.CheckCertificate(certificate);
+                    Assert.IsTrue(response.IsValid);
+                }
+                else {
+                    Console.WriteLine("{0} ThreadCertificateCheck number:{1} certificate:1", DateTime.Now, i);
+                    X509Certificate2 certificate = new X509Certificate2("Resources/Certificates/Revoked.cer");
+                    RevocationResponse response = crlLookup.CheckCertificate(certificate);
+                    Assert.IsFalse(response.IsValid);
+                }
+                Console.WriteLine("{0} ThreadCertificateCheck number:{1} done", DateTime.Now, i);
+            }
         }
     }
 }
