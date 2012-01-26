3b63137c0b0c16637d4cb8dd199dfe9fd26ab684
jacob.lund.mogensen <jacob.lund.mogensen@4e58de57-8926-0410-947e-8945c843cdd7>
2012-01-26 12:04:36 +0000

    And project that can send to the live production test endpoint
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@18994 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ samples/ProductionUddi/Oioubl_Invoice_LiveTestEndpoint/Oioubl_Invoice_EndpointLiveTestEndpont.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace dk.gov.oiosi.samples.TestUddi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using dk.gov.oiosi.samples.ClientExample;
+
+    public class Oioubl_Invoice_EndpointOfficient : AbstractRaspClient
+    {
+
+        public Oioubl_Invoice_EndpointOfficient()
+        {
+
+            //this.client = new OiosiRaspClient(UddiType.Production, "./Resources/xml/ProductionUddi/123.xml");
+            this.client = new OiosiRaspClient(UddiType.Production, "./Resources/xml/ProductionUddi/OIOUBL_Invoice_v2p2.xml");
+        }
+
+        static void Main(string[] args)
+        {
+            new Oioubl_Invoice_EndpointOfficient().Perform();
+            Console.WriteLine("Finish");
+            Console.ReadKey();
+        }
+    }
+}
