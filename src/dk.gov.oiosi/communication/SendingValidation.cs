6e34456b0ddae02f89d7dea9dda8fa889bd30584
jacob.lund.mogensen <jacob.lund.mogensen@4e58de57-8926-0410-947e-8945c843cdd7>
2012-02-02 10:57:41 +0000

    Add file
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@19026 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ src/dk.gov.oiosi/communication/SendingValidation.cs
@@ -0,0 +1,72 @@
+﻿/*
+  * The contents of this file are subject to the Mozilla Public
+  * License Version 1.1 (the "License"); you may not use this
+  * file except in compliance with the License. You may obtain
+  * a copy of the License at http://www.mozilla.org/MPL/
+  *
+  * Software distributed under the License is distributed on an
+  * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+  * or implied. See the License for the specific language governing
+  * rights and limitations under the License.
+  *
+  *
+  * The Original Code is .NET RASP toolkit.
+  *
+  * The Initial Developer of the Original Code is Accenture and Avanade.
+  * Portions created by Accenture and Avanade are Copyright (C) 2009
+  * Danish National IT and Telecom Agency (http://www.itst.dk). 
+  * All Rights Reserved.
+  *
+  * Contributor(s):
+  *   Gert Sylvest, Avanade
+  *   Jesper Jensen, Avanade
+  *   Ramzi Fadel, Avanade
+  *   Patrik Johansson, Accenture
+  *   Dennis Søgaard, Accenture
+  *   Christian Pedersen, Accenture
+  *   Martin Bentzen, Accenture
+  *   Mikkel Hippe Brun, ITST
+  *   Finn Hartmann Jordal, ITST
+  *   Christian Lanng, ITST
+  *   Jacob Mogensen, mySupply
+  * 
+  */
+
+namespace dk.gov.oiosi.communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using dk.gov.oiosi.configuration;
+    using dk.gov.oiosi.extension.wcf.Interceptor.Validation.Schema;
+    using System.Xml;
+    using dk.gov.oiosi.extension.wcf.Interceptor.Validation.Schematron;
+
+    public class SendingValidation
+    {
+
+        public bool Validate(OiosiMessage oiosiMessage)
+        {
+            bool result = true;
+            SendingOptionConfig sendingOptionConfig = ConfigurationHandler.GetConfigurationSection<SendingOptionConfig>();
+            
+            if (sendingOptionConfig.SchemaValidationBool)
+            {
+                SchemaValidatorWithLookup schemaValidatorWithLookup = new SchemaValidatorWithLookup();
+                XmlDocument xmlDocument = oiosiMessage.MessageXml;
+                schemaValidatorWithLookup.Validate(xmlDocument);
+                result = true;
+            }
+
+            if (result && sendingOptionConfig.SchematronValidationBool)
+            {
+                SchematronValidatorWithLookup schematronValidatorWithLookup = new SchematronValidatorWithLookup();
+                XmlDocument xmlDocument = oiosiMessage.MessageXml;
+                schematronValidatorWithLookup.Validate(xmlDocument);
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
