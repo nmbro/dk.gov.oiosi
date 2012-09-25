e91eea7ec349135b77d9cf25b07323d44445245e
s.p.nielsen <s.p.nielsen@4e58de57-8926-0410-947e-8945c843cdd7>
2012-09-25 11:44:47 +0000

    Minor fix to test build server. 1st.
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@19822 4e58de57-8926-0410-947e-8945c843cdd7

--- test/dk.gov.oiosi.test.unit/security/revocation/CrlLookupTest.cs
+++ test/dk.gov.oiosi.test.unit/security/revocation/CrlLookupTest.cs
@@ -13,7 +13,7 @@ namespace dk.gov.oiosi.test.unit.security.revocation
     {
         private string funkttionscertificat = "Resources/Certificates/CVR30808460.Expire20131101.FOCES1.pfx";
         private string medarbejdercertifikatRevoked = "Resources/Certificates/CVR30808460.Expire20130307.Test MOCES1 (medarbejdercertificat 2)(Spærret).pfx";
-        
+
         [TestFixtureSetUp]
         public void Setup()
         {
@@ -36,19 +36,22 @@ namespace dk.gov.oiosi.test.unit.security.revocation
             X509Certificate2 certificate = new X509Certificate2(medarbejdercertifikatRevoked, "Test1234");
             Assert.IsNotNull(certificate, "Test certificate was null.");
             RevocationResponse response = crlLookup.CheckCertificate(certificate);
-            if(response.Exception != null)
-            {
-                Assert.IsNull(response.Exception, "RevocationResponse contain an exception: " + response.Exception.ToString());
-            }
-            
-            Assert.IsFalse(response.IsValid, "The revoked certifikate was valid.");
+
+            Console.WriteLine("Not before:" + certificate.NotBefore.ToString());
+            Console.WriteLine("Not after:" + certificate.NotAfter.ToString());
+            Console.WriteLine("Subject" + certificate.Subject);
+            Console.WriteLine("Issuer" + certificate.Issuer);
+
+            Assert.IsFalse(response.IsValid, "The revoked certifikate was valid");
         }
 
         [Test]
-        public void LookTestMultiThread() {
+        public void LookTestMultiThread()
+        {
             List<Thread> threads = new List<Thread>();
             CrlLookup crlLookup = new CrlLookup();
-            for (int i = 0; i < 32; i++) {
+            for (int i = 0; i < 32; i++)
+            {
                 Thread thread = new Thread(ThreadCertificateCheck);
                 thread.Start();
                 threads.Add(thread);
@@ -56,23 +59,28 @@ namespace dk.gov.oiosi.test.unit.security.revocation
 
             Predicate<Thread> isDead = delegate(Thread t) { return !t.IsAlive; };
 
-            while (!threads.TrueForAll(isDead)) {
+            while (!threads.TrueForAll(isDead))
+            {
                 Thread.Sleep(TimeSpan.FromSeconds(1));
             }
         }
 
-        private void ThreadCertificateCheck() {
+        private void ThreadCertificateCheck()
+        {
             CrlLookup crlLookup = new CrlLookup();
-            for (int i = 0; i < 16; i++) {
+            for (int i = 0; i < 16; i++)
+            {
                 Random random = new Random();
                 int select = random.Next(2);
-                if (select < 1) {
+                if (select < 1)
+                {
                     Console.WriteLine("{0} ThreadCertificateCheck number:{1} certificate:0", DateTime.Now, i);
                     X509Certificate2 certificate = new X509Certificate2(funkttionscertificat, "Test1234");
                     RevocationResponse response = crlLookup.CheckCertificate(certificate);
                     Assert.IsTrue(response.IsValid);
                 }
-                else {
+                else
+                {
                     Console.WriteLine("{0} ThreadCertificateCheck number:{1} certificate:1", DateTime.Now, i);
                     X509Certificate2 certificate = new X509Certificate2(medarbejdercertifikatRevoked, "Test1234");
                     RevocationResponse response = crlLookup.CheckCertificate(certificate);
