0a50ba55889ab3e2afe25f59a37ccf7c91e3df20
jacob.lund.mogensen <jacob.lund.mogensen@4e58de57-8926-0410-947e-8945c843cdd7>
2014-07-01 10:28:42 +0000

    Split AssemblyInfo up, so it can be reused for all projects
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@20308 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ AssemblyInfoCommon.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Digitaliseringsstyrelsen")]
+[assembly: AssemblyProduct("RASP .Net")]
+[assembly: AssemblyCopyright("Copyright © DIGST 2007-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
---
+++ AssemblyInfoVersion.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+// We use the format: major.minor.patch. Must be automatic updated by the build server
+[assembly: AssemblyVersion("2.1.0.0")]
+
+// right-click -> properties -> Product Version. Must be automatic updated by the build server
+[assembly: AssemblyInformationalVersionAttribute("2.1 DEV")]
