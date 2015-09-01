7981b58417c00b065df0a79be7d9e5a036564d77
Jacob Mogensen <Jacob Mogensen@4e58de57-8926-0410-947e-8945c843cdd7>
2015-09-01 15:49:15 +0000

    remove endpoint test. The test is know performed in NHR, and not in RASP.
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@21497 4e58de57-8926-0410-947e-8945c843cdd7

--- test/dk.gov.oiosi.test.unit/common/UtilitiesTest.cs
+++ test/dk.gov.oiosi.test.unit/common/UtilitiesTest.cs
@@ -23,126 +23,126 @@ namespace dk.gov.oiosi.test.unit.common
             _searcher = new DocumentTypeConfigSearcher();
         }
 
-        [Test]
-        public void TestIdentifierEAN() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_EAN);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual(new IdentifierEan("5798009811578").KeyTypeCode, code);
+        ////[Test]
+        ////public void TestIdentifierEAN() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_EAN);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual(new IdentifierEan("5798009811578").KeyTypeCode, code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(IdentifierEan), identifier.GetType());
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(IdentifierEan), identifier.GetType());
 
-        }
+        ////}
         
-        [Test]
-        public void TestIdentifierOVT() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_OVT);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("ovt", code);
+        ////[Test]
+        ////public void TestIdentifierOVT() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_OVT);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("ovt", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(Identifier), identifier.GetType());
-        }
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(Identifier), identifier.GetType());
+        ////}
 
-        [Test]
-        public void TestIdentifierCVR() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_CVR);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("cvr", code);
+        ////[Test]
+        ////public void TestIdentifierCVR() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_CVR);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("cvr", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(IdentifierCvr),identifier.GetType());
-        }
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(IdentifierCvr),identifier.GetType());
+        ////}
 
-        [Test]
-        public void TestIdentifierP() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_P);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("p", code);
+        ////[Test]
+        ////public void TestIdentifierP() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_P);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("p", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(Identifier), identifier.GetType());
-        }
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(Identifier), identifier.GetType());
+        ////}
 
-        [Test]
-        public void TestIdentifierSE() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_SE);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("se", code);
+        ////[Test]
+        ////public void TestIdentifierSE() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_SE);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("se", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(Identifier), identifier.GetType());
-        }
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(Identifier), identifier.GetType());
+        ////}
 
-        [Test]
-        public void TestIdentifierVANS() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_VANS);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("vans", code);
+        ////[Test]
+        ////public void TestIdentifierVANS() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_VANS);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("vans", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(Identifier), identifier.GetType());
-        }
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(Identifier), identifier.GetType());
+        ////}
 
-        [Test]
-        public void TestIdentifierIBAN() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_IBAN);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("iban", code);
+        ////[Test]
+        ////public void TestIdentifierIBAN() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_IBAN);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("iban", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(Identifier), identifier.GetType());
-        }
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(Identifier), identifier.GetType());
+        ////}
 
-        [Test]
-        public void TestIdentifierDUNS() {
-            XmlDocument document = new XmlDocument();
-            document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_DUNS);
-            DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
-            string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
-            Assert.AreEqual("duns", code);
+        ////[Test]
+        ////public void TestIdentifierDUNS() {
+        ////    XmlDocument document = new XmlDocument();
+        ////    document.Load(TestConstants.PATH_INVOICE_XML_IDENTIFIER_DUNS);
+        ////    DocumentTypeConfig documentType = _searcher.FindUniqueDocumentType(document);
+        ////    string code = Utilities.GetEndpointKeyTypeCode(document, documentType);
+        ////    Assert.AreEqual("duns", code);
 
-            string keyXpath = documentType.EndpointType.Key.XPath;
-            PrefixedNamespace[] namespaces = documentType.Namespaces;
+        ////    string keyXpath = documentType.EndpointType.Key.XPath;
+        ////    PrefixedNamespace[] namespaces = documentType.Namespaces;
 
-            Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
-            Assert.AreEqual(typeof(Identifier), identifier.GetType());
-        }     
+        ////    Identifier identifier = Utilities.GetEndpointKeyByXpath(document, keyXpath, namespaces, code);
+        ////    Assert.AreEqual(typeof(Identifier), identifier.GetType());
+        ////}     
           
     }
 }
