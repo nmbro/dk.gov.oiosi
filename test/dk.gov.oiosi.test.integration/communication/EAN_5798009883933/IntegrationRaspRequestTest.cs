c33db7363f9837dbc6385543f6a00316f914bb90
Jacob Mogensen <Jacob Mogensen@4e58de57-8926-0410-947e-8945c843cdd7>
2016-09-20 08:02:08 +0000

    add communication test
    git-svn-id: https://svn.softwareboersen.dk/openebusiness/dk.gov.oiosi/library/dotnet/trunk@33586 4e58de57-8926-0410-947e-8945c843cdd7

---
+++ test/dk.gov.oiosi.test.integration/communication/EAN_5798009883933/IntegrationRaspRequestTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml;
+using dk.gov.oiosi.addressing;
+using dk.gov.oiosi.common;
+using dk.gov.oiosi.common.cache;
+using dk.gov.oiosi.communication;
+using dk.gov.oiosi.communication.configuration;
+using dk.gov.oiosi.configuration;
+using dk.gov.oiosi.raspProfile.communication;
+using dk.gov.oiosi.security;
+using dk.gov.oiosi.security.ldap;
+using dk.gov.oiosi.security.oces;
+using dk.gov.oiosi.security.revocation;
+using dk.gov.oiosi.uddi;
+using dk.gov.oiosi.xml.documentType;
+using dk.gov.oiosi.xml.xpath;
+using NUnit.Framework;
+using dk.gov.oiosi.security.lookup;
+
+namespace dk.gov.oiosi.test.integration.communication.EAN_5798009883933
+{
+
+    [TestFixture]
+    public class IntegrationRaspRequestTest : AbstractIntegrationRaspRequestTest
+    {
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            //CertificateLoader loader = new CertificateLoader();
+            //this.ClientCertificate = loader.GetCertificateFromStoreWithSerialNumber("4C 8C F7 64", StoreLocation.CurrentUser, StoreName.My);
+            this.ClientCertificate = CertificateUtil.InstallAndGetOces2FunctionCertificateFromCertificateStore();
+
+            //X509Certificate2 clientCertificate2 = CertificateUtil.InstallAndGetOces2FunctionCertificateFromCertificateStore();
+
+            ConfigurationUtil.SetupConfiguration("Resources/RaspConfiguration.Live.xml");
+        }
+
+        [Test]
+        public void OioublInvoice201MustBeSendableByRaspRequest()
+        {
+            AssertSendable("Resources/Documents/EAN_5798009883933/OIOUBL_Invoice_v2p1.xml");
+        }       
+    }
+}
